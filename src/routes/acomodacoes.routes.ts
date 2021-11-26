14bd1b8766728985916b64d8a71b11c7a106963b
Dayane Mendes <day.neisca@gmail.com>
2021-11-26 19:19:48 -0300

    Adicioando rota para cadastro

--- src/routes/acomodacoes.routes.ts
+++ src/routes/acomodacoes.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import * as acomodacoesController from "../controllers/acomodacoes.controller";
+import multer from "multer";
+import uploads from "../config/upload";
 
 export const router = Router();
 export const path = "/acomodacoes";
+export const upload = multer(uploads);
 
-router.get("/", acomodacoesController.listarAcomodacoes);
-router.get("/:id", acomodacoesController.acomodacaoID);
-router.post("/acomodacoes", acomodacoesController.criar);
-
-
+router.get(path, acomodacoesController.listarAcomodacoes);
+router.get(path + "/:id", acomodacoesController.acomodacaoID);
+router.post(path, acomodacoesController.criar);
+router.post(path, upload.single("imagem"), acomodacoesController.criar);
